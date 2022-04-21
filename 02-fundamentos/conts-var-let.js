989a0c46f250cf0b04d293b3b5573379c39b0efc
Sergio Pereira Jorquera <sergiopereira.jorquera@gmail.com>
2022-04-21 15:58:00 -0400

    dos

---
+++ 02-fundamentos/conts-var-let.js
@@ -0,0 +1,11 @@
+ nombre = 'Wolverine'; //creada de forma global
+
+if (true){
+    nombre = 'Magneto'
+};
+
+console.log(nombre);
+
+//const: no se pueden cambiar
+//var: creada de forma global
+//let: puede cambiar valor
